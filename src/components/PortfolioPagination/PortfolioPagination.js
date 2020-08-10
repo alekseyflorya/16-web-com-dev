75f3762c2daef23935ee022b68967887de86ff2c
alekseyflorya <aleksey.florya@gmail.com>
2020-08-10 04:42:41 +0300

    10-08-2020 4:45

--- src/components/PortfolioPagination/PortfolioPagination.js
+++ src/components/PortfolioPagination/PortfolioPagination.js
@@ -4,11 +4,16 @@ import {Container} from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import {Link} from "react-router-dom";
 import {PricesButton} from "../UI/ButtonLink";
+import PortfolioData from "../PortfolioArray";
 
 function PortfolioPagination() {
+  console.log(window.location.pathname)
+  const idx = PortfolioData.findIndex((item) => item.path === window.location.pathname);
+  console.log(idx)
+  const projectsCount = PortfolioData.length;
 
-  const prevLInk = ''
-  const nextLink = ''
+  const prevLink = idx-1 < 0 ? PortfolioData[projectsCount-1].path : PortfolioData[idx-1].path;
+  const nextLink = idx+1 >= projectsCount ? PortfolioData[0].path : PortfolioData[idx+1].path;
 
   return (
     <section className={classes.PortfolioPagination}>
@@ -17,13 +22,13 @@ function PortfolioPagination() {
           <Grid item container sm />
           <Grid item container sm={6} xs={12}>
             <Grid item sm={6} className={classes.LinkBox}>
-              <Link to={prevLInk} className={`${classes.Link} ${classes.PrevLInk}`} />
+              <Link to={prevLink} className={`${classes.Link} ${classes.PrevLInk}`} />
             </Grid>
             <Grid item sm={6} className={classes.LinkBox}>
-              <Link to={prevLInk} className={`${classes.Link} ${classes.NextLink}`} />
+              <Link to={nextLink} className={`${classes.Link} ${classes.NextLink}`} />
             </Grid>
           </Grid>
-          <Grid item container sm>
+          <Grid item container sm className={classes.BtnBox}>
             <PricesButton title={'Вернуться назад'} path={'/portfolio'} />
           </Grid>
         </Grid>
